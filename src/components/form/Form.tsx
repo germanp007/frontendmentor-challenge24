8f1ebbd71107478a56af7e397d71352e708f5e07
germanp007 <pintogerman281@gmail.com>
2024-04-08 21:24:03 -0300

    creando validacion de email

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,14 +1,40 @@
+import { FormEvent, useState } from "react";
+
 const Form = () => {
+  const [email, setEmail] = useState<string>("");
+
+  const handleEmail = (e: FormEvent<HTMLFormElement> | string) => {
+    if (typeof e === "string") {
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      const isValidEmail = regex.test(e);
+      if (isValidEmail) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
   return (
     <div>
-      <form className="flex flex-col items-center">
+      <form
+        className="flex flex-col items-center"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleEmail(email);
+        }}
+      >
         <input
           type="email"
           name="email"
+          value={email}
           placeholder="Enter your email..."
           className="w-[318px] border border-VeryDarkBlue h-11 rounded-sm px-5 mb-4 focus:outline-none"
+          onChange={(e) => setEmail(e.target.value)}
         />
-        <button className="bg-BrightBlue text-white w-[318px] h-11 rounded-sm">
+        <button
+          type="submit"
+          className="bg-BrightBlue text-white w-[318px] h-11 rounded-sm"
+        >
           Get Started
         </button>
       </form>
