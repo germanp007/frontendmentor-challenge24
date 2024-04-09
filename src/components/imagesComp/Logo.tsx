43b5c132ba748db451dbf55bffaeeeab8a61e00f
germanp007 <pintogerman281@gmail.com>
2024-04-09 14:35:55 -0300

    terminando footer

--- src/components/imagesComp/Logo.tsx
+++ src/components/imagesComp/Logo.tsx
@@ -1,12 +1,6 @@
-import { FC } from "react";
 import logo from "../../../public/images/logo.svg";
 
-type Props = {
-  image: string;
-  name: string;
-};
-
-const LogoComponent: FC<Props> = ({ image, name }) => (
+const LogoComponent = () => (
   <img src={logo} alt="Logo" className="h-[30px] w-[100px]" />
 );
 export default LogoComponent;
