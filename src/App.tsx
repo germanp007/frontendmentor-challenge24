a3666ad163d36f0ada7e4ad4cf95dd7e1d24fbcc
germanp007 <pintogerman281@gmail.com>
2024-04-08 21:04:43 -0300

    ajustando layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import Form from "./components/form/Form";
 import HeroImage from "./components/hero/HeroImage";
 import Navbar from "./components/nav/Navbar";
-import imageTwo from "../public/images/illustration-2.svg";
-import arrow from "../public/images/icon-arrow.svg";
+import Section from "./components/section2/Section";
+
 const App = () => {
   return (
     <>
@@ -30,34 +30,7 @@ const App = () => {
               backgroundImage: "url(/public/images/bg-curve-mobile.svg)",
             }}
           ></div>
-          <section className="bg-LightGrayishBlue w-full min-h-[300px] py-1 pb-5">
-            <img
-              src={imageTwo}
-              alt="image of people working together"
-              className="w-[90%] h-auto mx-auto mb-[7rem]"
-            />
-            <div>
-              <h2 className="font-Header text-[1.1rem] text-VeryDarkBlue font-semibold text-center mb-8">
-                {" "}
-                Stay productive, wherever you are
-              </h2>
-              <p className="  text-VeryDarkBlue text-left w-[318px] m-auto">
-                Never let location be an issue when accessing your files. Fylo
-                has you covered for all of your file storage needs. <br />{" "}
-                <br /> Securely share files and folders with friends, family and
-                colleagues for live collaboration. No email attachments
-                required!
-              </p>
-              <div className="w-[174px] m-auto border-b-[1px] border-ModerateCyan">
-                <a href="#" className="flex justify-between items-center gap-2">
-                  <p className="text-ModerateCyan">See How Fylo works</p>
-
-                  <img src={arrow} alt="arrow" />
-                </a>
-              </div>
-              {/* <hr className="border-ModerateCyan border-1 w-[170px] m-auto" /> */}
-            </div>
-          </section>
+          <Section />
         </article>
       </main>
     </>
